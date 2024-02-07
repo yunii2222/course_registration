745325bd353fefb76c98f4d6786bd164d3256244
ppyye <6076hs@gmail.com>
2024-02-07 14:55:44 +0900

    RegistDTO 작성

--- course_registration/src/main/java/com/ohgiraffers/student/registManager/model/dto/RegistDTO.java
+++ course_registration/src/main/java/com/ohgiraffers/student/registManager/model/dto/RegistDTO.java
@@ -1,4 +1,49 @@
 package com.ohgiraffers.student.registManager.model.dto;
 
 public class RegistDTO {
+    private int studentCode;
+    private int lectureCode;
+    private String registDate;
+
+    public RegistDTO() {
+    }
+
+    public RegistDTO(int studentCode, int lectureCode, String registDate) {
+        this.studentCode = studentCode;
+        this.lectureCode = lectureCode;
+        this.registDate = registDate;
+    }
+
+    public int getStudentCode() {
+        return studentCode;
+    }
+
+    public void setStudentCode(int studentCode) {
+        this.studentCode = studentCode;
+    }
+
+    public int getLectureCode() {
+        return lectureCode;
+    }
+
+    public void setLectureCode(int lectureCode) {
+        this.lectureCode = lectureCode;
+    }
+
+    public String getRegistDate() {
+        return registDate;
+    }
+
+    public void setRegistDate(String registDate) {
+        this.registDate = registDate;
+    }
+
+    @Override
+    public String toString() {
+        return "RegistDTO{" +
+                "studentCode=" + studentCode +
+                ", lectureCode=" + lectureCode +
+                ", registDate='" + registDate + '\'' +
+                '}';
+    }
 }
