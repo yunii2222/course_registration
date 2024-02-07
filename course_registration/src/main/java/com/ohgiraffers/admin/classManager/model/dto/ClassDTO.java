4a391de8aaaebcebbf635457c083dc73e057b5c7
1bfish106 <1bfish106@naver.com>
2024-02-07 14:55:12 +0900

    커밋

--- course_registration/src/main/java/com/ohgiraffers/admin/classManager/model/dto/ClassDTO.java
+++ course_registration/src/main/java/com/ohgiraffers/admin/classManager/model/dto/ClassDTO.java
@@ -1,4 +1,83 @@
 package com.ohgiraffers.admin.classManager.model.dto;
 
 public class ClassDTO {
+
+    private  int classCode;
+    private String className;
+    private  String category;
+    private int classGrade;
+    private String classCredit;
+    private String classStatus;
+
+    public ClassDTO() {
+    }
+
+    public ClassDTO(int classCode, String className, String category, int classGrade, String classCredit, String classStatus) {
+        this.classCode = classCode;
+        this.className = className;
+        this.category = category;
+        this.classGrade = classGrade;
+        this.classCredit = classCredit;
+        this.classStatus = classStatus;
+    }
+
+    public int getClassCode() {
+        return classCode;
+    }
+
+    public void setClassCode(int classCode) {
+        this.classCode = classCode;
+    }
+
+    public String getClassName() {
+        return className;
+    }
+
+    public void setClassName(String className) {
+        this.className = className;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public int getClassGrade() {
+        return classGrade;
+    }
+
+    public void setClassGrade(int classGrade) {
+        this.classGrade = classGrade;
+    }
+
+    public String getClassCredit() {
+        return classCredit;
+    }
+
+    public void setClassCredit(String classCredit) {
+        this.classCredit = classCredit;
+    }
+
+    public String getClassStatus() {
+        return classStatus;
+    }
+
+    public void setClassStatus(String classStatus) {
+        this.classStatus = classStatus;
+    }
+
+    @Override
+    public String toString() {
+        return "ClassDTO{" +
+                "classCode=" + classCode +
+                ", className='" + className + '\'' +
+                ", category='" + category + '\'' +
+                ", classGrade=" + classGrade +
+                ", classCredit='" + classCredit + '\'' +
+                ", classStatus='" + classStatus + '\'' +
+                '}';
+    }
 }
--- course_registration/src/main/java/com/ohgiraffers/admin/professorManager/model/dto/ProfessorDTO.java
+++ course_registration/src/main/java/com/ohgiraffers/admin/professorManager/model/dto/ProfessorDTO.java
@@ -1,4 +1,60 @@
 package com.ohgiraffers.admin.professorManager.model.dto;
 
 public class ProfessorDTO {
+    private int professorCode;
+    private String professorNmae;
+    private String professorPhone;
+    private int subjectCode;
+
+    public ProfessorDTO() {
+    }
+
+    public ProfessorDTO(int professorCode, String professorNmae, String professorPhone, int subjectCode) {
+        this.professorCode = professorCode;
+        this.professorNmae = professorNmae;
+        this.professorPhone = professorPhone;
+        this.subjectCode = subjectCode;
+    }
+
+    public int getProfessorCode() {
+        return professorCode;
+    }
+
+    public void setProfessorCode(int professorCode) {
+        this.professorCode = professorCode;
+    }
+
+    public String getProfessorNmae() {
+        return professorNmae;
+    }
+
+    public void setProfessorNmae(String professorNmae) {
+        this.professorNmae = professorNmae;
+    }
+
+    public String getProfessorPhone() {
+        return professorPhone;
+    }
+
+    public void setProfessorPhone(String professorPhone) {
+        this.professorPhone = professorPhone;
+    }
+
+    public int getSubjectCode() {
+        return subjectCode;
+    }
+
+    public void setSubjectCode(int subjectCode) {
+        this.subjectCode = subjectCode;
+    }
+
+    @Override
+    public String toString() {
+        return "ProfessorDTO{" +
+                "professorCode=" + professorCode +
+                ", professorNmae='" + professorNmae + '\'' +
+                ", professorPhone='" + professorPhone + '\'' +
+                ", subjectCode=" + subjectCode +
+                '}';
+    }
 }
